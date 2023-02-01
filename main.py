eedc9ce926e7cc6a6b3390deee2677098b2438b3
Warhair <36608769+Warhair@users.noreply.github.com>
2023-02-01 12:14:53 +0100

    eredmenyelsoreszemegirva

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 'Megoldás'
 
-def eredmény():
-    pass
+def eredmény(j_pont, g_pont):
+    if j_pont > 21 and g_pont > 21:
+        print("Döntetlen")
+    elif j_pont > 21:
+        print("játékos vesztet")
+    elif g_pont >21:
+        print("Gép vesztet")
 
 'Tesztek'
