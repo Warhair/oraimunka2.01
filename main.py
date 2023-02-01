174e31ac7462bd30cf0a23d74ec7b475b340c97d
Warhair <36608769+Warhair@users.noreply.github.com>
2023-02-01 12:31:56 +0100

    oszegzesmetoduskesz

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 'Megoldás'
+jatekos = [1,2,3]
+gep = [4,5,6,7,7]
 
-def eredmény(j_pont, g_pont):
-    if j_pont > 21 and g_pont > 21:
+def osszegzes(lapok):
+    osszeg = 0
+    for i in lapok:
+        osszeg += i
+
+    return osszeg
+
+
+def eredmény(jatekos, gep):
+    if jatekos > 21 and gep > 21:
         print("Döntetlen")
-    elif j_pont > 21:
+    elif jatekos > 21:
         print("játékos vesztet")
-    elif g_pont >21:
+    elif gep >21:
         print("Gép vesztet")
 
 'Tesztek'
+jatekososz = osszegzes(jatekos)
+gepo = osszegzes(gep)
+eredmény(jatekososz, gepo)
