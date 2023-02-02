0b8d5d16649961ecf3e7ea547ee2092e582a23b9
Warhair <36608769+Warhair@users.noreply.github.com>
2023-02-02 12:50:03 +0100

    tesztekmegirva2.0

--- main.py
+++ main.py
@@ -27,7 +27,11 @@ def eredmény(jatekos: list, gep: list):
 def testek():
     jatekos_vesztett_teszt()
     gep_vesztett_teszt()
-    dontetlen_teszt()
+    dontetlen_mindaketten_vesztet_teszt()
+    kozelebb21jatekosnyert_teszt()
+    kozelebb21gepnyert_teszt()
+    dontetlen_eseten_jatekos_nyert_teszt()
+    dontetlen_eseten_gep_nyert_teszt()
 def jatekos_vesztett_teszt():
     jatekos = [7, 8, 9]
     gep = [3, 5, 6, 4]
@@ -51,7 +55,7 @@ def gep_vesztett_teszt():
         print("Teszt megbukott")
 
 
-def dontetlen_teszt():
+def dontetlen_mindaketten_vesztet_teszt():
     jatekos = [7, 8, 10]
     gep = [3, 5, 6, 10]
     kapott = eredmény(jatekos, gep)
@@ -62,6 +66,47 @@ def dontetlen_teszt():
     else:
         print("teszt megbukott")
 
+def kozelebb21jatekosnyert_teszt():
+    jatekos = [7, 8, 5]
+    gep = [3, 5, 10]
+    kapott = eredmény(jatekos, gep)
+    vart = "Játékos nyert"
+    print("közelebb 21-hez játekos nyert")
+    if kapott == vart:
+        print("Teszt sikeres")
+    else:
+        print("teszt megbukott")
 
+def kozelebb21gepnyert_teszt():
+    jatekos = [7, 8, 4]
+    gep = [3, 7, 10]
+    kapott = eredmény(jatekos, gep)
+    vart = "Gép nyert"
+    print("közelebb 21-hez gép nyert")
+    if kapott == vart:
+        print("Teszt sikeres")
+    else:
+        print("teszt megbukott")
 
+def dontetlen_eseten_jatekos_nyert_teszt():
+    jatekos = [7, 8, 4]
+    gep = [3, 5, 6, 4]
+    kapott = eredmény(jatekos, gep)
+    vart = "Játékos nyert"
+    print("Döntetlen esetén játékos nyert")
+    if kapott == vart:
+        print("Teszt sikeres")
+    else:
+        print("teszt megbukott")
+
+def dontetlen_eseten_gep_nyert_teszt():
+    jatekos = [3, 4, 5, 7]
+    gep = [7, 8, 4]
+    kapott = eredmény(jatekos, gep)
+    vart = "Gép nyert"
+    print("Döntetlen esetén Gép nyert")
+    if kapott == vart:
+        print("Teszt sikeres")
+    else:
+        print("teszt megbukott")
 testek()
